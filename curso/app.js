221caf3c1e2f66b6461fa4fb0f29847234865652
Yo <yoelnacho@gmail.com>
2016-04-20 02:16:44 -0300

    Creación estructura curso

---
+++ curso/app.js
@@ -0,0 +1,92 @@
+(function() {
+    // Nueva forma de encapsular
+    var app = angular.module('nav', []);
+
+    // Navegación
+    app.controller('navCtrl', ['$scope', function($scope) {
+        $scope.hola = "hola mundo";
+
+        $scope.navecacionSelected = 1;
+
+        $scope.navegacion = [
+            {
+                id:1,
+                nombre:"Ejercicio 1",
+                descripcion: [
+                    "Leer los datos de un modelo.",
+                    "filtros  uppercase, lowercase y currency"
+                ]
+            },
+            {
+                id:2,
+                nombre:"Ejercicio 2",
+                descripcion: [
+                    "Reemplazar datos del modelo desde un controlador.",
+                    "Utilización de animate.css para la directiva ng-show",
+                    "Utilización de angular.copy, para copiar el estado de una variable a otra"
+                ]
+            },
+            {
+                id:3,
+                nombre:"Ejercicio 3",
+                descripcion: [
+                    "Funciones encapsuladas correctamente.",
+                    "Cambio de forma al escribir un controlador (para evitar errores en la minificación)",
+                    "ng-repeat de un arreglo compuesto"
+                ]
+            },
+            {
+                id:4,
+                nombre:"Ejercicio 4",
+                descripcion: [
+                    "Filtros para ng-repeat",
+                    "Ordenar ng-repeat por orderBy"
+                ]
+            },
+            {
+                id:5,
+                nombre:"Ejercicio 5",
+                descripcion: [
+                    "$http",
+                    "Leer datos de un .json"
+                ]
+            },
+            {
+                id:6,
+                nombre:"Ejercicio 6",
+                descripcion: [
+                    "ng-include"
+                ]
+            },
+            {
+                id:7,
+                nombre:"Ejercicio 7",
+                descripcion: [
+                    "Detectando IPs, País, Ciudad y otra información Online"
+                ]
+            },
+            {
+                id:8,
+                nombre:"Ejercicio 8",
+                descripcion: [
+                    "API externa, clima mundo",
+                    "Funión para convertir K a C",
+                    "Incluir una función dentro de un parseo",
+                    "utilizando filtro date (fechas/horas)"
+                ]
+            },
+            {
+                id:9,
+                nombre:"Ejercicio 9",
+                descripcion: [
+                    "ng-route",
+                    "$routeProvider dependencia",
+                    "anidando contoladores",
+                    "ng-class"
+                ]
+            }
+
+        ];
+    }]);
+
+})();
